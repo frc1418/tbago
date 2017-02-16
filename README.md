0eed41cd46b602d6a63c475695b5ff82fcfe7780
ErikBoesen <erik.boesen@gmail.com>
2017-02-16 15:52:25 -0500

    Add GetEvent request function and README content

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# go-bluealliance
+# tba for Go
 A Go library for interfacing with The Blue Alliance's APIv2.
+
+To install `tba`:
+
+    go get github.com/CarlColglazier/tba
+
+Then, to use it in your project:
+
+```go
+import (
+    "github.com/CarlColglazier/tba"
+)
+```
+
+## License
+This software is available under the terms of the [MIT License](LICENSE).
--- requests.go
+++ requests.go
@@ -18,6 +18,13 @@ func (tba Client) GetTeam(team int) (t Team, e error) {
 	return t, e
 }
 
+// Event request
+func (tba Client) GetEvent(key string) (ev Event, e error) {
+	url := fmt.Sprintf("event/%s", key)
+	e = tba.jsonToStruct(url, &ev)
+	return ev, e
+}
+
 // Team events request
 func (tba Client) GetTeamEvents(team, year int) (events []Event, e error) {
 	url := fmt.Sprintf("team/frc%d/%d/events", team, year)
